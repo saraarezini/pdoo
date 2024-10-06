9efeed3d09a43bd756ebdd851ae226757c6d7ea8
USUARIO <USUARIO@LAPTOP-7QC4HA27.home>
2024-10-06 14:03:35 +0200

    Creación de Trigo, atributos clase e instancia. Closes #2

---
+++ myfirstproject/src/myfirstproject/Trigo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package myfirstproject;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Trigo {
+    public static final String ICONO = "/img/trigo.png";
+    public static final int DURACION =2;
+    public static final int EXPERIENCIA = 1;
+    public static final int PRECIO_VENTA = 2;
+    public static final int RIEGO = 10;
+    
+    
+    private String icono;
+    private int duracionProduccion;
+    private int experienciaRecolecta;
+    private int precioVenta;
+    private int riego;
+   
+    
+}
